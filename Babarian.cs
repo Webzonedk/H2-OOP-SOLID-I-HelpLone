06c3bb04ec4ad746db06c73c4b6508f7759a2927
Kent Pedersen <webzone@kpdesign.dk>
2021-06-11 09:30:06 +0200

    Add project files.

---
+++ Babarian.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpLone
+{
+    public class Babarian : ICharacter, IMeleeAttack
+    {
+        public void Bash()
+        {
+            Console.WriteLine("I'm bashing someone");
+        }
+
+        public void Cleave()
+        {
+            Console.WriteLine("I'm cleaving someone");
+        }
+
+        public void Die()
+        {
+            Console.WriteLine("I'm dying");
+        }
+
+        public void Fight()
+        {
+            Console.WriteLine("I'm fighting");
+        }
+
+        public void Heal()
+        {
+            Console.WriteLine("I'm healing");
+        }
+
+        public void Slash()
+        {
+            Console.WriteLine("I'm slashing someone");
+        }
+
+
+    }
+
+}
